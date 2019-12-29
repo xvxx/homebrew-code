1291b9eac08dc19bc498cf5d99b9f80fdf943d31
ʇʞʌp <c@dvkt.io>
2019-12-29 01:08:44 -0800

    typo

--- todo.rb
+++ todo.rb
@@ -5,7 +5,7 @@ class Todo < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/dvkt/todo/releases/download/v1.0.0/todo-v0.0.0-macos.zip"
+    url "https://github.com/dvkt/todo/releases/download/v1.0.0/todo-v1.0.0-macos.zip"
     sha256 "f6b2e8c007b3cbd6143bab2fba2b32b44e63063800e3c48acef72f5cd32b8732"
   elsif OS.linux?
     url "https://github.com/dvkt/todo/releases/download/v1.0.0/todo-v1.0.0-linux-x86_64.tar.gz"
