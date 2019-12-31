b207664c8cc76c9954875603b91fec01f04496b0
dvkt <c@dvkt.io>
2019-12-31 01:11:12 -0800

    phetch v0.1.5

--- phetch.rb
+++ phetch.rb
@@ -1,18 +1,19 @@
 class Phetch < Formula
   desc "quick lil gopher client"
-  homepage "https://github.com/dvkt/phetch/"
-  version "0.1.4"
+  homepage "https://github.com/dvkt/phetch"
+  version "v0.1.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/dvkt/phetch/releases/download/v0.1.4/phetch-v0.1.4-macos.zip"
-    sha256 "a188e9690a11be0245dcbb5f11c33d1a60d6cd84aded8d0a878fa223b493e52b"
+    url "https://github.com/dvkt/phetch/releases/download/v0.1.5/phetch-v0.1.5-macos.zip"
+    sha256 "7c67148eab8fc7c36d5dfcd29e55e6edb5463fc3dc739eb2a7e9728a27f0fee1"
   elsif OS.linux?
-    url "https://github.com/dvkt/phetch/releases/download/v0.1.4/phetch-v0.1.4-linux-x86_64.tar.gz"
-    sha256 "bca9da1d4d5202def6f6703c44d636fdc1ac415486f0b66a9e2754d0be64d69d"
+    url "https://github.com/dvkt/phetch/releases/download/v0.1.5/phetch-v0.1.5-linux-x86_64.tar.gz"
+    sha256 "5b0c40ba8e9615adf61ae7f8e9f4141956e1314a2f88b37e5ed2dbc210eb3e93"
   end
 
   def install
     bin.install "phetch"
   end
 end
+
