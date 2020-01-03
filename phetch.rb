6eb191f1ebca1d5094eaa5040c00c0a595e06e7d
dvkt <c@dvkt.io>
2020-01-03 13:52:12 -0800

    phetch v0.1.9

--- phetch.rb
+++ phetch.rb
@@ -6,10 +6,10 @@ class Phetch < Formula
 
   if OS.mac?
     url "https://github.com/dvkt/phetch/releases/download/v0.1.9/phetch-v0.1.9-macos.zip"
-    sha256 "b451ffe0e16c5b20ed9231a3951846de76488c2487aac80661f054effb866f11"
+    sha256 "b8f2e1904700d0139ca3da0197b04328fdcffe7c6542a78f76a2a4714936d3fb"
   elsif OS.linux?
     url "https://github.com/dvkt/phetch/releases/download/v0.1.9/phetch-v0.1.9-linux-x86_64.tar.gz"
-    sha256 "42b57230c137781492f98508019a9912eb3c027c9cdbb66de18a4cc3d79ab462"
+    sha256 "11246979eafe40e0608d068d6bd708ae018638266c784e97647e84a0572ef849"
   end
 
   def install
