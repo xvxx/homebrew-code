39310e16ac434c52bceff596f3134e698f372a49
dvkt <c@dvkt.io>
2020-01-04 16:30:51 -0800

    phd vv0.1.5

--- phd.rb
+++ phd.rb
@@ -1,15 +1,15 @@
 class Phd < Formula
   desc "an esoteric gopher server"
   homepage "https://github.com/dvkt/phd"
-  version "v0.1.4"
+  version "v0.1.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/dvkt/phd/releases/download/v0.1.4/phd-v0.1.4-macos.zip"
-    sha256 "013e0a7141996f5430ebe22c132ca17566a9598956f9c080d146c5819dad1d42"
+    url "https://github.com/dvkt/phd/releases/download/v0.1.5/phd-v0.1.5-macos.zip"
+    sha256 "37dafeb71b6cf9655f0c211c36bdad36148d604316b7b1142e400669c7d3de2a"
   elsif OS.linux?
-    url "https://github.com/dvkt/phd/releases/download/v0.1.4/phd-v0.1.4-linux-x86_64.tar.gz"
-    sha256 "0a32fb22f95c75d644447a1afffafb0fe93cd1a165798f52b575dc58544d59ee"
+    url "https://github.com/dvkt/phd/releases/download/v0.1.5/phd-v0.1.5-linux-x86_64.tar.gz"
+    sha256 "713535729f0c86640590ccee1ebabcc3742787f7f642b50c3be1a77d60f815e0"
   end
 
   def install
