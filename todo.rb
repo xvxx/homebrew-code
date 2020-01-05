784d2d3a32392e1cd118e731c29694c49e0782e9
dvkt <c@dvkt.io>
2020-01-04 17:32:52 -0800

    todo v1.0.1

--- todo.rb
+++ todo.rb
@@ -5,11 +5,11 @@ class Todo < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/dvkt/ldpl-todo/releases/download/v1.0.0/todo-v1.0.0-macos.zip"
-    sha256 "f6b2e8c007b3cbd6143bab2fba2b32b44e63063800e3c48acef72f5cd32b8732"
+    url "https://github.com/dvkt/ldpl-todo/releases/download/v1.0.1/todo-v1.0.1-macos.zip"
+    sha256 "30ce1e510275e923fc69fa271a48c44d0e56adf44ce0ba55cbe0c211926cbb90"
   elsif OS.linux?
-    url "https://github.com/dvkt/ldpl-todo/releases/download/v1.0.0/todo-v1.0.0-linux-x86_64.tar.gz"
-    sha256 "e80030a90b995a2e83532629c9988f4042a28d1bcbc561da11e1569f33b79380"
+    url "https://github.com/dvkt/ldpl-todo/releases/download/v1.0.1/todo-v1.0.1-linux-x86_64.tar.gz"
+    sha256 "b45c7145f6d617a44c80cadbc3074f496679b9ed2d11d6e63e133ab66c021a50"
   end
 
   def install
