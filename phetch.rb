4906ab022aaa8a30efef01c37437a8d7ce31530a
dvkt <c@dvkt.io>
2020-01-08 19:15:37 -0800

    phetch v0.1.12

--- phetch.rb
+++ phetch.rb
@@ -1,19 +1,22 @@
 class Phetch < Formula
   desc "quick lil gopher client"
   homepage "https://github.com/dvkt/phetch"
-  version "v0.1.11"
+  version "v0.1.12"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/dvkt/phetch/releases/download/v0.1.11/phetch-v0.1.11-macos.zip"
-    sha256 "bc8e87484debd20a45049ae19bdb8aa1923772c93bc45187822e8be39a658c6c"
+    url "https://github.com/dvkt/phetch/releases/download/v0.1.12/phetch-v0.1.12-macos.zip"
+    sha256 "b3f786b577b73c4437f2b64e5a2ed43a19c6ae2db15a04ff111b45341e2de4ed"
   elsif OS.linux?
-    url "https://github.com/dvkt/phetch/releases/download/v0.1.11/phetch-v0.1.11-linux-x86_64.tgz"
-    sha256 "906a21a558da942bfa83739756b187a95cc0056c180c20ced4512c65a4383f22"
+    url "https://github.com/dvkt/phetch/releases/download/v0.1.12/phetch-v0.1.12-linux-x86_64.tgz"
+    sha256 "852b222a6fe91a5cf06e40113f23f22448d53695cf266585c3d9cc0ae63a20c1"
   end
 
   def install
     bin.install "phetch"
+    if File.exists? "phetch.1"
+      man.install "phetch.1"
+    end
   end
 end
 
