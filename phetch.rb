0067bc7049ae1c4dabdbc98e4abf12b4f3e51a28
chris west <c@xvxx.io>
2020-09-26 13:07:06 -0700

    phetch v1.0.4

--- phetch.rb
+++ phetch.rb
@@ -1,15 +1,15 @@
 class Phetch < Formula
   desc "quick lil gopher client"
   homepage "https://github.com/xvxx/phetch"
-  version "v1.0.3"
+  version "v1.0.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/xvxx/phetch/releases/download/v1.0.3/phetch-v1.0.3-macos.zip"
-    sha256 "c7971012ac652cd2a0b84f0b36a4653ec9007f4379ef62789d20367457430117"
+    url "https://github.com/xvxx/phetch/releases/download/v1.0.4/phetch-v1.0.4-macos.zip"
+    sha256 "7e2fbca128d76bb8321f1089336f59bf0a1905455bc1ee4e6cdaba5b63e7b210"
   elsif OS.linux?
-    url "https://github.com/xvxx/phetch/releases/download/v1.0.3/phetch-v1.0.3-linux-x86_64.tgz"
-    sha256 "ff313a11f9e259385b53a84cd50a4d430bc2c7c3b8e89da930fc5fe14349d673"
+    url "https://github.com/xvxx/phetch/releases/download/v1.0.4/phetch-v1.0.4-linux-x86_64.tgz"
+    sha256 "344adad56c4c6c0b0a49d937036fa84213bfe4b80c3474d3577de02bbb4d5606"
   end
 
   def install
