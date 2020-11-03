7675054a677ed8e4d1fbd9125d1a3547540c4aba
chris west <c@xvxx.io>
2020-11-03 15:28:14 -0800

    shy v0.1.10

--- shy.rb
+++ shy.rb
@@ -1,15 +1,15 @@
 class Shy < Formula
   desc "bare-bones ssh launcher"
   homepage "https://github.com/xvxx/shy"
-  version "v0.1.9"
+  version "v0.1.10"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/xvxx/shy/releases/download/v0.1.9/shy-v0.1.9-macos.zip"
-    sha256 "e77b73dc9a6403501a171cee2abeeea8a043af918189ce5efde1f5f1009b7841"
+    url "https://github.com/xvxx/shy/releases/download/v0.1.10/shy-v0.1.10-macos.zip"
+    sha256 "ee4be6f0dec32160562ccfb198cd646678fe445ce9a0fe78aced6106c1cc9441"
   elsif OS.linux?
-    url "https://github.com/xvxx/shy/releases/download/v0.1.9/shy-v0.1.9-linux-x86_64.tgz"
-    sha256 "e4402b6d80b7e2174765c82abb9d31dda3dfc3bf414291d9844519d8c8559ca0"
+    url "https://github.com/xvxx/shy/releases/download/v0.1.10/shy-v0.1.10-linux-x86_64.tgz"
+    sha256 "f104a400cf3e598e91dc356572bd206085cddc18c8e2643eef067ebe1d8095e2"
   end
 
   def install
