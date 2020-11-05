f787a6fa9218275b4ce176544d4666505f225430
chris west <c@xvxx.io>
2020-11-04 18:42:19 -0800

    deadwiki v0.1.24

--- deadwiki.rb
+++ deadwiki.rb
@@ -1,15 +1,15 @@
 class Deadwiki < Formula
   desc "dead simple personal wiki"
   homepage "https://github.com/xvxx/deadwiki"
-  version "v0.1.23"
+  version "v0.1.24"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/xvxx/deadwiki/releases/download/v0.1.23/deadwiki-v0.1.23-macos.zip"
-    sha256 "3f11893ab08880cce23bc5eb6e6b436389383651f64c996d08e8f4ca7c8056e6"
+    url "https://github.com/xvxx/deadwiki/releases/download/v0.1.24/deadwiki-v0.1.24-macos.zip"
+    sha256 "1fcc2b29d46db4809ecbe785b58183685e05698a7558364e9fa349348b40151e"
   elsif OS.linux?
-    url "https://github.com/xvxx/deadwiki/releases/download/v0.1.23/deadwiki-v0.1.23-linux-x86_64.tgz"
-    sha256 "226b843e5f117b20fb2485c78c119e6128b9faf92316cb84de15fcb3e66db34d"
+    url "https://github.com/xvxx/deadwiki/releases/download/v0.1.24/deadwiki-v0.1.24-linux-x86_64.tgz"
+    sha256 "b2845384b7091d994f95001833e3968a68c363321394e7f7f97374768137ce00"
   end
 
   def install
