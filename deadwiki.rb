9566f972360a17b33692839b5f6d76ffaf77805a
chris west <c@xvxx.io>
2020-11-11 13:05:22 -0800

    deadwiki v0.1.25

--- deadwiki.rb
+++ deadwiki.rb
@@ -1,19 +1,19 @@
 class Deadwiki < Formula
   desc "dead simple personal wiki"
   homepage "https://github.com/xvxx/deadwiki"
-  version "v0.1.24"
+  version "v0.1.25"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/xvxx/deadwiki/releases/download/v0.1.24/deadwiki-v0.1.24-macos.zip"
-    sha256 "1fcc2b29d46db4809ecbe785b58183685e05698a7558364e9fa349348b40151e"
+    url "https://github.com/xvxx/deadwiki/releases/download/v0.1.25/deadwiki-v0.1.25-macos.zip"
+    sha256 "f95ea4b67c0d9c0c8991c764ac841468a8e0c8e464a81ffea39fe5c8fbc61f1c"
   elsif OS.linux?
-    url "https://github.com/xvxx/deadwiki/releases/download/v0.1.24/deadwiki-v0.1.24-linux-x86_64.tgz"
-    sha256 "b2845384b7091d994f95001833e3968a68c363321394e7f7f97374768137ce00"
+    url "https://github.com/xvxx/deadwiki/releases/download/v0.1.25/deadwiki-v0.1.25-linux-x86_64.tgz"
+    sha256 "dcf952f019d208164098267d01ed0da27fc40149ed3e255580e26cdae66352b4"
   end
 
   def install
-    bin.install "dead"
+    bin.install "deadwiki"
     if File.exists? "deadwiki.1"
       man.install "deadwiki.1"
     end
