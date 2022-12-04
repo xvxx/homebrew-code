a28d8c14bef4e4663e31b17d6ddccc233ff7e301
chris west <c@xvxx.io>
2022-12-04 15:22:40 -0800

    phd v0.1.15

--- phd.rb
+++ phd.rb
@@ -1,14 +1,14 @@
 class Phd < Formula
   desc "an esoteric gopher server"
   homepage "https://github.com/xvxx/phd"
-  version "v0.1.14"
+  version "v0.1.15"
 
   if OS.mac?
-    url "https://github.com/xvxx/phd/releases/download/v0.1.14/phd-v0.1.14-macos.zip"
-    sha256 "1abd11595ea257db0bf3399dd5ed3ff4cb4fb557fdbc828aadbbfeb0231112bd"
+    url "https://github.com/xvxx/phd/releases/download/v0.1.15/phd-v0.1.15-macos.zip"
+    sha256 "38f9b7cfff555a46a5e030f35468c469ee7dcdcd1342ad4160c5383830765986"
   elsif OS.linux?
-    url "https://github.com/xvxx/phd/releases/download/v0.1.14/phd-v0.1.14-linux-x86_64.tgz"
-    sha256 "ff8c12173ce7c9908d5f459e1305032621fc30c7200121945544e97481cb546c"
+    url "https://github.com/xvxx/phd/releases/download/v0.1.15/phd-v0.1.15-linux-x86_64.tgz"
+    sha256 "c080bab39111df3297ab3fe429b755bfbe586641409010086b4c47d11405d216"
   end
 
   def install
